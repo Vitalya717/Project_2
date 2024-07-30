1fd0d78ca69306cf57bd50a35b85bc102ab46ffe
Vitalya Yakovlev <vitaalya717@mail.ru>
2024-07-30 22:14:48 +0300

    src

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 14_518;
+        int oneMile = 20;
+        int bonusMiles = ticketPrice / oneMile;
+        System.out.println("Начислено миль:" + bonusMiles);
+
+    }
+}
